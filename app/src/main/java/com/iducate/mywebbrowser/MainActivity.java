a78c23aeeb35bdb72b49312d12fd68f44bb024e1
Martin Knudsen <martinknud@gmail.com>
2015-12-08 14:53:17 +0100

    added some comments

--- app/src/main/java/com/iducate/mywebbrowser/MainActivity.java
+++ app/src/main/java/com/iducate/mywebbrowser/MainActivity.java
@@ -10,8 +10,11 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
+		//get our web view
 		WebView browser=(WebView)findViewById(R.id.webkit);
+		
+		//load the URL
 		browser.loadUrl(getIntent().getDataString());
 	}
 
